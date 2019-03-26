699483227ed7ce8d0e7c1824ae03e0b394efe054
Colin Thomas <cthom187@uncc.edu>
2019-03-26 02:39:29 +0000

    added courses to mathclass and sompsciclass models. Created a 'back' link to the welcome screen on both compsciclasses view and mathclasses view

--- app/controllers/compsciclasses_controller.rb
+++ app/controllers/compsciclasses_controller.rb
@@ -1,2 +1,8 @@
 class CompsciclassesController < ApplicationController
+    def index
+        @compsciclasses = CompSciClass.all
+    end
+    def show
+        @compsciclasses = CompSciClass.all
+    end
 end
